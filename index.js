5b657012a0517540d87f91fb707ddea743ace636
unknown <rishab.saddiq0607@gmail.com>
2021-04-06 17:47:40 +0100

    token removed

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const TOKEN = process.env.TOKEN;
 const PREFIX = ''
 
 bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
-  });
-
-  bot.on('message', msg => {
-    if (msg.content === '_uptime') {
-      msg.reply(`The server uptime is ${os.uptime()} Seconds`);
-    }
+    console.info(`Logged in as ${bot.user.tag}! Ready For Action!`);
   });
 
   bot.on('message', msg => {
@@ -21,12 +15,36 @@ bot.on('ready', () => {
     }
   })
 
-  bot.on('message', msg => {
-    if(msg.content === '_ping'){
-      msg.channel.send('Pong! '+"`"+(Date.now() -
-       msg.createdTimestamp)+'ms'+"`");
-    }
-  })
 
+  bot.on('message', msg => {
+    if (msg.content === '_uptime') {
+      msg.channel.send({embed: {
+       color:  3447003,
+       description: `**the Server Uptime is ${os.uptime} seconds**`,
+     }})
+    }
+  });
+
+  bot.on('message', msg => {
+    if (msg.content === '_ping') {
+      msg.channel.send({embed: {
+       color:  3447003,
+       description: ('```Pong! '+""+(Date.now() -
+       msg.createdTimestamp)+'ms'+"```")
+     }})
+    }
+  });
+
+  bot.on('message', msg => {
+    if (msg.content === '_help') {
+      msg.channel.send({embed: {
+       color:  3447003,
+       description: '```You Need Help i See🤓\n _uptime to see uptime, _ping to see ping````'
+       
+     }})
+    }
+  });
+  
+  
   
 bot.login(TOKEN);
